b3f892a37cc0a8c345ea7e6632e68e6c8a831aa4
EriccLiu <1486587187@qq.com>
2018-12-24 11:27:48 +0800

    将js生成页面转化为静态页面

--- WebContent/js/yjjyjczc.js
+++ WebContent/js/yjjyjczc.js
@@ -37,24 +37,14 @@ $(function () {
         // 引入SimpleMarker，loadUI的路径参数为模块名中 'ui/' 之后的部分
         AMapUI.loadUI(['overlay/SimpleMarker'], function(SimpleMarker) {
             // 启动页面
-            initPage(SimpleMarker);	// 加载特勤站
-            loadPS();				// 加载永久站
-            loadSS(SimpleMarker);	// 加载小型站
+
+            loadWS(SimpleMarker);	// 加载水源
             loadMS(SimpleMarker);	// 加载微型站
-            loadWS(SimpleMarker);	// 加载小型站
+            loadSS(SimpleMarker);	// 加载小型站
             loadSoR(SimpleMarker);	// 加载危险源
+            loadPS();				// 加载永久站
+            initPage(SimpleMarker);	// 加载特勤站
         });
-        
-        // 引入SvgMarker，loadUI的路径参数为模块名中 'ui/' 之后的部分
-        //AMapUI.loadUI(['overlay/SvgMarker'], function(SvgMarker) {
-            // 启动页面
-        	//initPage(SvgMarker);	// 加载特勤站
-            //loadPS();				// 加载永久站
-            //loadSS(SvgMarker);	// 加载小型站
-            //loadMS(SvgMarker);	// 加载微型站
-            //loadWS(SvgMarker);	// 加载小型站
-            //loadSoR(SvgMarker);	// 加载危险源
-        //});
 
         // 哈尔滨消防支队
         window.center = new AMap.Marker({
@@ -272,7 +262,6 @@ $(function () {
             listenMarkerList(SoRMarkerList);
             map.add(SoRMarkerList);
         }
-
         
         
         // 增加点列表的监听
@@ -552,30 +541,7 @@ $(function () {
     // echart_3
     // 信息分类控制
     function echart_3() {
-    	var myChart = document.getElementById("bottom_left");
-    	var tags = ["特勤站", "永久站", "小型站", "微型站", "水源"];
     	
-    	for(i = 0; i < tags.length; i++){
-    		var div_outer = document.createElement("div");
-    		var text = document.createElement("div");
-    		var input = document.createElement("input");
-    		
-    		div_outer.setAttribute("class", "chart_3_button");
-    		text.setAttribute("class", "chart_3_text");
-    		input.setAttribute("type", "checkbox");
-    		input.setAttribute("name", "o_" + i);
-    		input.setAttribute("class", "chart_3_c3");
-    		input.setAttribute("checked", "");
-    		input.setAttribute("simpleswitch", "11");
-    		input.setAttribute("result", "true");
-    		input.setAttribute("style", "display: none;");
-    		
-    		text.innerHTML = tags[i];
-    		
-    		div_outer.appendChild(text);
-    		div_outer.appendChild(input);
-    		myChart.appendChild(div_outer);
-    	}
     }
     
     $(".chart_3_c3").simpleSwitch();
@@ -673,121 +639,18 @@ $(function () {
     // bottom_center
     // 和5一起组成火灾报警处置
     function echart_4() {
-    	var myChart = document.getElementById("bottom_center");
-    	var text = document.createElement("p");
-    	var alarm = document.createElement("div");
-    	var alarm_icon = document.createElement("div");
-    	var setting = document.createElement("div");
-    	
-    	// 火情单位情况HTML组件
-    	text.setAttribute("class", "display_text");
-    	text.setAttribute("style", "font-size:18px");
-    	
-    	text.innerHTML = "火情单位：哈尔滨敖麓谷雅酒店\t";
+    	var text = document.getElementById("chart4_text");
+    	text.innerHTML += "new火情单位：哈尔滨敖麓谷雅酒店\t";
     	text.innerHTML += "地址：哈尔滨市松北区创新三路800号<br\>";
     	text.innerHTML += "消防安全负责人：吕绍芬\t";
     	text.innerHTML += "联系方式：138898976541\t";
-    	
-    	// 火情图标组件
-    	alarm_icon.setAttribute("style", "float:left");
-    	var level_1 = document.createElement("div");
-    	var level_2 = document.createElement("div");
-    	var level_3 = document.createElement("div");
-    	var level_4 = document.createElement("div");
-    	var level_up = document.createElement("div");
-    	
-    	level_1.setAttribute("class", "chart_4_level");
-    	level_2.setAttribute("class", "chart_4_level");
-    	level_3.setAttribute("class", "chart_4_level");
-    	level_4.setAttribute("class", "chart_4_level");
-    	level_up.setAttribute("class", "chart_4_level");
-    	
-    	var level_1_icon = document.createElement("span");
-    	var level_2_icon = document.createElement("span");
-    	var level_3_icon = document.createElement("span");
-    	var level_4_icon = document.createElement("span");
-    	var level_up_icon = document.createElement("span");
-    	
-    	level_1_icon.setAttribute("class", "glyphicon glyphicon-exclamation-sign chart_4_icon");
-    	level_1_icon.setAttribute("style", "font-size:70px; color:blue; padding-left:10px");
-　　		level_1_icon.setAttribute("aria-hidden", "true");
-    	level_2_icon.setAttribute("class", "glyphicon glyphicon-exclamation-sign chart_4_icon");
-　　		level_2_icon.setAttribute("aria-hidden", "true");
-    	level_2_icon.setAttribute("style", "font-size:70px; color:yellow; padding-left:10px");
-    	level_3_icon.setAttribute("class", "glyphicon glyphicon-exclamation-sign chart_4_icon");
-　　		level_3_icon.setAttribute("aria-hidden", "true");
-    	level_3_icon.setAttribute("style", "font-size:70px; color:orange; padding-left:10px");
-    	level_4_icon.setAttribute("class", "glyphicon glyphicon-exclamation-sign chart_4_icon");
-　　		level_4_icon.setAttribute("aria-hidden", "true");
-    	level_4_icon.setAttribute("style", "font-size:70px; color:red; padding-left:10px");
-    	level_up_icon.setAttribute("class", "glyphicon glyphicon-upload chart_4_icon");
-　　		level_up_icon.setAttribute("aria-hidden", "true");
-    	level_up_icon.setAttribute("style", "font-size:70px; color:#00FF00; padding-left:10px");
-　　		
-    	var level_1_text = document.createElement("p");
-    	var level_2_text = document.createElement("p");
-    	var level_3_text = document.createElement("p");
-    	var level_4_text = document.createElement("p");
-    	var level_up_text = document.createElement("p");
-    	
-    	level_1_text.setAttribute("style", "font-size:20px");
-    	level_2_text.setAttribute("style", "font-size:20px");
-    	level_3_text.setAttribute("style", "font-size:20px");
-    	level_4_text.setAttribute("style", "font-size:20px");
-    	level_up_text.setAttribute("style", "font-size:20px");
-    	
-    	level_1_text.innerHTML = "一级火情";
-    	level_2_text.innerHTML = "二级火情";
-    	level_3_text.innerHTML = "三级火情";
-    	level_4_text.innerHTML = "四级火情";
-    	level_up_text.innerHTML = "火情升级";
-    	
-    	level_1.appendChild(level_1_icon);
-    	level_1.appendChild(level_1_text);
-    	level_2.appendChild(level_2_icon);
-    	level_2.appendChild(level_2_text);
-    	level_3.appendChild(level_3_icon);
-    	level_3.appendChild(level_3_text);
-    	level_4.appendChild(level_4_icon);
-    	level_4.appendChild(level_4_text);
-    	level_up.appendChild(level_up_icon);
-    	level_up.appendChild(level_up_text);
-    	
-    	alarm_icon.appendChild(level_1);
-    	alarm_icon.appendChild(level_2);
-    	alarm_icon.appendChild(level_3);
-    	alarm_icon.appendChild(level_4);
-    	alarm_icon.appendChild(level_up);
-    	
-    	// 配置调整HTML组件
-    	setting.setAttribute("class", "chart_4_setting");
-    	var tags = ["一般火情预案配置调整", "重大危险源预案配置调整"];
-    	for (i = 0; i < tags.length; i++){
-        	var div = document.createElement("div");
-        	div.setAttribute("id", "chart_4_setting"+ (i+1));
-        	div.setAttribute("class", "demo-row col-xs-3");
-        	div.setAttribute("style", "width: 270px !important");
-        	var a = document.createElement("a");
-      　　　　		a.setAttribute("href", "#fakelink");      　　　　			
-      　　　　		a.setAttribute("class", "btn btn-block btn-lg btn-primary");
-      　　　　		a.setAttribute("style", "padding: 5px 16px; border: 2px solid white");
-      　　　　		a.innerHTML = tags[i];
-      　　　　		div.appendChild(a);
-      　　　　		setting.appendChild(div);
-        }
-    	
-    	alarm.appendChild(alarm_icon);
-    	alarm.appendChild(setting);
-    	
-    	// 将组件添加到chart中
-    	myChart.appendChild(text);
-    	myChart.appendChild(alarm);
     }
     
     // right_2
     // 当前站点状态
     function echart_6(){
     	// 基于准备好的dom，初始化echarts实例
+    	/*
         var parent = document.getElementById('bottom_right');
         var tags = ["车辆统计情况", "装备统计情况", "器材统计情况", "重点岗位实时监控", "重大危险源统计"];
 
@@ -803,6 +666,7 @@ $(function () {
       　　　　		div.appendChild(a);
       　　　　		parent.appendChild(div);
         }
+        */
     }
     
     // right_1
